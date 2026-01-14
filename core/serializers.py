373b46907b4dc3739f5dbc2d1e6e13c3861d7e62
nuckysss <sitso.nkrumah@gmail.com>
2026-01-14 20:36:08 +0000

    Huh

--- core/serializers.py
+++ core/serializers.py
@@ -26,7 +26,8 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
         data["username"] = self.user.username
-        data["email"] = self.user.email
         data["role"] = self.user.role
+        data["email"] = self.user.email
+        
 
         return data
