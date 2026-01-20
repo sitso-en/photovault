83fa2fb9a645167a8250ad2f017ab2b06b1b8db0
nuckysss <sitso.nkrumah@gmail.com>
2026-01-20 19:19:01 +0000

    Yo

--- core/views.py
+++ core/views.py
@@ -6,8 +6,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
-    permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
